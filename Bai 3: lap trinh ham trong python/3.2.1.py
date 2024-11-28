2fa911aa1cdcda3ea22294d57b4e1b9bcca5ca5a
Phan Thế Đạt <Phanthedatfc37@gmail.com>
2024-11-28 14:10:45 +0700

    Update and rename 3.1 to 3.2.1.py

---
+++ Bai 3: lap trinh ham trong python/3.2.1.py
@@ -0,0 +1,10 @@
+print ("Ho ten: Phan The dat")
+print ("MSSV: 235752021610094")
+
+def sum(a, b):
+    
+    print("Tong hai so a va b la: " +str(a+b))
+a=int(input("Nhap a: "))
+b=int(input("Nhap b: "))
+sum(a,b)
+
