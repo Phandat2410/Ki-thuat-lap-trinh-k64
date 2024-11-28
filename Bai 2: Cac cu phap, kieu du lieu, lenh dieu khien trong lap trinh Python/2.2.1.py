e64c7c8e171b1968aefaa3f9f2ea9be4a61b2de7
Phan Thế Đạt <Phanthedatfc37@gmail.com>
2024-11-28 13:54:01 +0700

    Create 2.2.1.py

---
+++ Bai 2: Cac cu phap, kieu du lieu, lenh dieu khien trong lap trinh Python/2.2.1.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Phan The Dat")
+print("MSSV: 235752021610094")
+
+n1=int(input ("nhap so n1: "))
+n2=int(input ("Nhap so n2: "))
+if n1>n2:
+    print("n1 la so lon nhat")
+else:
+    print("n2 la so lon nhat")
