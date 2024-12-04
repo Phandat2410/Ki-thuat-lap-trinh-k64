0c956b00cb22a9532af86128f52d7f946de803a7
Phan Thế Đạt <Phanthedatfc37@gmail.com>
2024-12-04 23:26:28 +0700

    Create 6.2.1.py

---
+++ Bài 6. Lập trình hướng đối tượng trong Python/6.2.1.py
@@ -0,0 +1,17 @@
+print ("Họ tên: Phan Thế Đạt")
+print ("MSSV: 235752021610094")
+
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+# Tạo một đối tượng hình tròn với bán kính là 5
+circle = Circle(5)
+
+# Tính và in ra diện tích của hình tròn
+print("Diện tích hình tròn:", circle.area())
